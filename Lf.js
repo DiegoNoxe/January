f67f76756156e245b531bccc2d639008fba7b2df
Diego <Diegofla10@outlook.com>
2024-12-31 17:11:36 -0300

    .
    .

--- Lf.js
+++ Lf.js
@@ -3,9 +3,9 @@ const segundos = () => {
     const startDate = new Date(2007, 0, 10, 19, 50, 0);
     const now = new Date();
     const diferenca = now - startDate;
-    const seconds = Math.floor(diferenca / 1000);
+    const seconds = Math.floor(diferenca / 1000).toLocaleString('pt-BR');
     const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24)); 
-    clock.innerHTML = `Segundos de vida: ${seconds}! Quantidade de Dias atual: ${dias}!`;
+    clock.innerHTML = `Você viveu até agora:${seconds} segundos! Isso equivale a ${dias} repletos de histórias`;
 };
 
 segundos();
