af664dd2c21343f2c8a563ef1593e23b2eced449
Diego <Diegofla10@outlook.com>
2025-01-02 12:56:04 -0300

    .
    .

---
+++ Pages/Lf.js
@@ -0,0 +1,16 @@
+const segundos = () => 
+{
+    const clock = document.getElementsByClassName('clock')[0];
+    const startDate = new Date(2007, 0, 10, 19, 50, 0);
+    const now = new Date();
+    const diferenca = now - startDate;
+    const seconds = Math.floor(diferenca / 1000).toLocaleString('pt-BR');
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24)); 
+    clock.innerHTML = `Você viveu até agora: ${seconds} segundos!<br>Isso equivale a ${dias} repletos dias de histórias!`;
+};
+
+segundos();
+
+setInterval(() => {
+    segundos();
+}, 1000);
